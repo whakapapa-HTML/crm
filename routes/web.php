37d4b58ba0695ad7c9902933a1202e4633634c88
whakapapa-HTML <uriah.heep0810@gmail.com>
2024-01-15 22:55:51 +0900

    defineEmit

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,13 @@ Route::get(
     }
 );
 
+Route::get(
+    '/component-test',
+    function () {
+        return Inertia::render('ComponentTest');
+    }
+);
+
 Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
 Route::get('/inertia/create', [InertiaTestController::class, 'create'])->name('inertia.create');
 Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
