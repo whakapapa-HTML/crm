4d2a65050d6e5bd9646d39e31e79b5191f13ef70
whakapapa-HTML <uriah.heep0810@gmail.com>
2024-01-20 23:51:59 +0900

    Itemテストデータ

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -35,7 +35,7 @@ class ItemController extends Controller
      * @param  \App\Http\Requests\StoreItemRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreItemRequest $request)
+    public function store( $request)
     {
         //
     }
