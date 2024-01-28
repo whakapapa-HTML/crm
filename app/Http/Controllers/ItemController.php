199e5aeb25f4fcb9d6180a22be0cb9885b7e4b6c
whakapapa-HTML <uriah.heep0810@gmail.com>
2024-01-28 23:44:35 +0900

    Create　リンク作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -16,7 +16,7 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Items/Index',[
+        return Inertia::render('Items/Index', [
             'items' => Item::select('id', 'name', 'price', 'is_selling')->get()
         ]);
     }
@@ -28,7 +28,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
